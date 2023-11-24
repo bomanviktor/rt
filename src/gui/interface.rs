7777a44044bf53c83d34abee63ff2fddbfe07ab2
Salam Foon <78011597+salam-github@users.noreply.github.com>
2023-11-24 16:07:09 +0200

    add scroll bar, add css, fix bugs
    adding button functionality and experiment with layouts for the object sections (add cylinder has a new layout i'm testing)

--- src/gui/interface.rs
+++ src/gui/interface.rs
@@ -2,18 +2,17 @@ use glib::clone;
 use glib::signal::Inhibit;
 use gtk::prelude::*;
 use gtk::{
-    Box as GtkBox, Button, ComboBoxText, Entry, Orientation, Scale, Separator, Window, WindowType,
+    Box as GtkBox, Button, ComboBoxText, CssProvider, Entry, Orientation, Scale, Separator, Window,
+    WindowType,
 };
 use std::cell::RefCell;
 use std::rc::Rc;
-
-// use crate::objects::sphere;
 
 struct AppState {
     spheres: Vec<SphereConfig>,
     cylinders: Vec<CylinderConfig>,
     cubes: Vec<CubeConfig>,
-    flat_planes: Vec<FlatPlaneConfig>,
+    flat_plane: Vec<FlatPlaneConfig>,
 }
 #[allow(dead_code)]
 struct ObjectConfig {
@@ -21,7 +20,7 @@ struct ObjectConfig {
     pos_y_entry: Rc<RefCell<Entry>>,
     pos_z_entry: Rc<RefCell<Entry>>,
     radius_entry: Rc<RefCell<Entry>>,
-    // material_selector: Rc<RefCell<ComboBoxText>>,
+    material_selector: Rc<RefCell<ComboBoxText>>,
 }
 
 struct SphereConfig {
@@ -45,7 +44,7 @@ struct CubeConfig {
     pos_x_entry: Rc<RefCell<Entry>>,
     pos_y_entry: Rc<RefCell<Entry>>,
     pos_z_entry: Rc<RefCell<Entry>>,
-    size_entry: Rc<RefCell<Entry>>,
+    radius_entry: Rc<RefCell<Entry>>,
     material_selector: Rc<RefCell<ComboBoxText>>,
 }
 
@@ -62,52 +61,75 @@ pub fn launch_gui() {
         spheres: Vec::new(),
         cylinders: Vec::new(),
         cubes: Vec::new(),
-        flat_planes: Vec::new(),
+        flat_plane: Vec::new(),
     }));
 
     gtk::init().expect("Failed to initialize GTK.");
 
-    // // let object_selector = Rc::new(RefCell::new(ComboBoxText::new()));
-    // let pos_x_entry = Rc::new(RefCell::new(Entry::new()));
-    // let pos_y_entry = Rc::new(RefCell::new(Entry::new()));
-    // let pos_z_entry = Rc::new(RefCell::new(Entry::new())); // Added Z position entry
-    // let radius_entry = Rc::new(RefCell::new(Entry::new()));
-    // // let material_selector = Rc::new(RefCell::new(ComboBoxText::new()));
-
-    // pos_x_entry.borrow().connect_changed(|entry| {
-    //     let text = entry.get_text().to_string();
-    //     println!("X Entry changed (fn launch_gui): {}", text);
-    // });
+    // Create a CSS provider
+    let provider = CssProvider::new();
+    provider
+        .load_from_path("src/gui/style.css")
+        .expect("Failed to load CSS");
 
     let window = Window::new(WindowType::Toplevel);
     window.set_title("Ray Tracing Settings");
-    window.set_default_size(600, 600);
+    window.set_default_size(900, 900);
+    window
+        .get_style_context()
+        .add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    let scrolled_window = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
 
     let vbox = GtkBox::new(Orientation::Vertical, 10);
+    vbox.set_border_width(10);
+    vbox.set_spacing(10);
+
     let vbox_clone = vbox.clone();
+
+    scrolled_window.add(&vbox);
+    window.add(&scrolled_window);
 
     let render_button = Button::with_label("Render picture");
     vbox.pack_start(&render_button, false, false, 0);
+    render_button
+        .get_style_context()
+        .add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     // Create a horizontal box for the side-by-side buttons
     let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
 
     let add_sphere_button = Button::with_label("Add Sphere");
     hbox.pack_start(&add_sphere_button, false, false, 0);
+    add_sphere_button
+        .get_style_context()
+        .add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     let add_cylinder_button = Button::with_label("Add Cylinder");
     hbox.pack_start(&add_cylinder_button, false, false, 0);
+    add_cylinder_button
+        .get_style_context()
+        .add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     let add_cube_button = Button::with_label("Add Cube");
     hbox.pack_start(&add_cube_button, false, false, 0);
+    add_cube_button
+        .get_style_context()
+        .add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     let add_flat_plane_button = Button::with_label("Add Flat Plane");
     hbox.pack_start(&add_flat_plane_button, false, false, 0);
+    add_flat_plane_button
+        .get_style_context()
+        .add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     // Add the horizontal box to the vertical box
     vbox.pack_start(&hbox, false, false, 0);
 
-    // let pos_x_entry = Rc::new(RefCell::new(Entry::new()));
+    let flow_box = gtk::FlowBox::new();
+    flow_box.set_valign(gtk::Align::Start);
+    flow_box.set_max_children_per_line(10); // Adjust as needed
+    flow_box.set_selection_mode(gtk::SelectionMode::None);
 
     add_sphere_button.connect_clicked(clone!(@strong vbox_clone, @strong app_state => move |_| {
         let sphere_count = app_state.borrow().spheres.len();
@@ -116,28 +138,26 @@ pub fn launch_gui() {
         vbox_clone.show_all();
     }));
 
-    add_cylinder_button.connect_clicked(clone!(@strong vbox_clone, @strong app_state => move |_| {
-        let cylinder_count = app_state.borrow().spheres.len();
+    add_cylinder_button.connect_clicked(clone!(@strong flow_box, @strong app_state => move |_| {
+        let cylinder_count = app_state.borrow().cylinders.len();
         let cylinder_section = create_cylinder_section(app_state.clone(), cylinder_count + 1);
-        vbox_clone.pack_start(&cylinder_section, false, false, 0);
-        vbox_clone.show_all();
+        flow_box.add(&cylinder_section);
+        flow_box.show_all();
     }));
 
     add_cube_button.connect_clicked(clone!(@strong vbox_clone, @strong app_state => move |_| {
-    let cube_count = app_state.borrow().cubes.len();
-    let cube_section = create_cube_section(app_state.clone(), cube_count + 1);
-    vbox_clone.pack_start(&cube_section, false, false, 0);
-    vbox_clone.show_all();
+        let cube_count = app_state.borrow().cubes.len();
+        let cube_section = create_cube_section(app_state.clone(), cube_count + 1);
+        vbox_clone.pack_start(&cube_section, false, false, 0);
+        vbox_clone.show_all();
     }));
 
-    add_flat_plane_button.connect_clicked(
-        clone!(@strong vbox_clone, @strong app_state => move |_| {
-        let plane_count = app_state.borrow().flat_planes.len();
-        let plane_section = create_flat_plane_section(app_state.clone(), plane_count + 1);
-        vbox_clone.pack_start(&plane_section, false, false, 0);
+    add_flat_plane_button.connect_clicked(clone!(@strong vbox_clone, @strong app_state => move |_| {
+        let flat_plane_count = app_state.borrow().flat_plane.len();
+        let flat_plane_section = create_flat_plane_section(app_state.clone(), flat_plane_count + 1);
+        vbox_clone.pack_start(&flat_plane_section, false, false, 0);
         vbox_clone.show_all();
-        }),
-    );
+    }));
 
     // Separator
     let separator = Separator::new(Orientation::Horizontal);
@@ -175,6 +195,7 @@ pub fn launch_gui() {
     let height_entry = Entry::new();
     height_entry.set_placeholder_text(Some("Height"));
     vbox.pack_start(&height_entry, false, false, 0);
+    vbox.pack_start(&flow_box, true, true, 0);
 
     // Render Button
     render_button.connect_clicked(clone!(@strong app_state => move |_| {
@@ -206,13 +227,13 @@ pub fn launch_gui() {
             let pos_x = cube.pos_x_entry.borrow().get_text().to_string();
             let pos_y = cube.pos_y_entry.borrow().get_text().to_string();
             let pos_z = cube.pos_z_entry.borrow().get_text().to_string();
-            let size = cube.size_entry.borrow().get_text().to_string();
+            let radius = cube.radius_entry.borrow().get_text().to_string();
             let material = cube.material_selector.borrow().get_active_text().unwrap_or_else(|| "Lambertian".into());
 
-            println!("Cube {}: X: {}, Y: {}, Z: {}, Size: {}, Material: {}", index + 1, pos_x, pos_y, pos_z, size, material);
+            println!("Cube {}: X: {}, Y: {}, Z: {}, Radius: {}, Material: {}", index + 1, pos_x, pos_y, pos_z, radius, material);
         }
 
-        for (index, flat_plane) in app_state_borrowed.flat_planes.iter().enumerate() {
+        for (index, flat_plane) in app_state_borrowed.flat_plane.iter().enumerate() {
             let pos_x = flat_plane.pos_x_entry.borrow().get_text().to_string();
             let pos_y = flat_plane.pos_y_entry.borrow().get_text().to_string();
             let pos_z = flat_plane.pos_z_entry.borrow().get_text().to_string();
@@ -225,7 +246,6 @@ pub fn launch_gui() {
 
     }));
 
-    window.add(&vbox);
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
@@ -251,10 +271,17 @@ fn validate_and_parse_entry(entry: &gtk::Entry, default_value: f64, label: &str)
 }
 
 fn create_sphere_section(app_state: Rc<RefCell<AppState>>, sphere_count: usize) -> gtk::Box {
+    let provider = CssProvider::new();
+    provider
+        .load_from_path("src/gui/style.css")
+        .expect("Failed to load CSS");
+
     let sphere_section = gtk::Box::new(gtk::Orientation::Horizontal, 0);
     let label_text = format!("Sphere {}", sphere_count);
     let sphere_label = gtk::Label::new(Some(&label_text));
     sphere_section.pack_start(&sphere_label, false, false, 0);
+    let style_context = sphere_label.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     let pos_x_entry = Entry::new();
     pos_x_entry.set_placeholder_text(Some("X Position"));
@@ -272,12 +299,23 @@ fn create_sphere_section(app_state: Rc<RefCell<AppState>>, sphere_count: usize) 
     radius_entry.set_placeholder_text(Some("Radius"));
     sphere_section.pack_start(&radius_entry, false, false, 0);
 
+    // Apply styles to entries
+    let entries = vec![&pos_x_entry, &pos_y_entry, &pos_z_entry, &radius_entry];
+    for entry in entries {
+        let style_context = entry.get_style_context();
+        style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
+    }
+
     let material_selector = ComboBoxText::new();
     material_selector.append_text("Lambertian");
     material_selector.append_text("Metal");
     material_selector.append_text("Dielectric");
     material_selector.set_active(Some(0));
     sphere_section.pack_start(&material_selector, false, false, 0);
+
+    // Apply styles to ComboBoxText
+    let style_context = material_selector.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
 
     let sphere_config = SphereConfig {
         pos_x_entry: Rc::new(RefCell::new(pos_x_entry)),
@@ -291,38 +329,62 @@ fn create_sphere_section(app_state: Rc<RefCell<AppState>>, sphere_count: usize) 
     sphere_section
 }
 
-fn create_cylinder_section(app_state: Rc<RefCell<AppState>>, cylinder_count: usize) -> gtk::Box {
-    let cylinder_section = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+fn create_cylinder_section(app_state: Rc<RefCell<AppState>>, cylinder_count: usize) -> gtk::Widget {
+    let provider = CssProvider::new();
+    provider
+        .load_from_path("src/gui/style.css")
+        .expect("Failed to load CSS");
+
+    let grid = gtk::Grid::new();
+    grid.set_column_spacing(10); // Adjust the spacing as needed
+
     let label_text = format!("Cylinder {}", cylinder_count);
     let cylinder_label = gtk::Label::new(Some(&label_text));
-    cylinder_section.pack_start(&cylinder_label, false, false, 0);
+    grid.attach(&cylinder_label, 0, 0, 1, 1); // Column 0, Row 0
 
     let pos_x_entry = Entry::new();
     pos_x_entry.set_placeholder_text(Some("X Position"));
-    cylinder_section.pack_start(&pos_x_entry, false, false, 0);
+    grid.attach(&pos_x_entry, 1, 0, 1, 1); // Column 1, Row 0
 
     let pos_y_entry = Entry::new();
     pos_y_entry.set_placeholder_text(Some("Y Position"));
-    cylinder_section.pack_start(&pos_y_entry, false, false, 0);
+    grid.attach(&pos_y_entry, 1, 1, 1, 1); // Column 1, Row 1
 
     let pos_z_entry = Entry::new();
     pos_z_entry.set_placeholder_text(Some("Z Position"));
-    cylinder_section.pack_start(&pos_z_entry, false, false, 0);
+    grid.attach(&pos_z_entry, 1, 2, 1, 1); // Column 1, Row 2
 
     let radius_entry = Entry::new();
     radius_entry.set_placeholder_text(Some("Radius"));
-    cylinder_section.pack_start(&radius_entry, false, false, 0);
+    grid.attach(&radius_entry, 1, 3, 1, 1); // Column 1, Row 3
 
     let height_entry = Entry::new();
     height_entry.set_placeholder_text(Some("Height"));
-    cylinder_section.pack_start(&height_entry, false, false, 0);
+    grid.attach(&height_entry, 1, 4, 1, 1); // Column 1, Row 4
+
+    // Apply styles to entries
+    let entries = vec![
+        &pos_x_entry,
+        &pos_y_entry,
+        &pos_z_entry,
+        &radius_entry,
+        &height_entry,
+    ];
+    for entry in entries {
+        let style_context = entry.get_style_context();
+        style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
+    }
 
     let material_selector = ComboBoxText::new();
     material_selector.append_text("Lambertian");
     material_selector.append_text("Metal");
     material_selector.append_text("Dielectric");
     material_selector.set_active(Some(0));
-    cylinder_section.pack_start(&material_selector, false, false, 0);
+    grid.attach(&material_selector, 1, 5, 1, 1); // Column 1, Row 5
+
+    // Apply styles to ComboBoxText
+    let style_context = material_selector.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
 
     let cylinder_config = CylinderConfig {
         pos_x_entry: Rc::new(RefCell::new(pos_x_entry)),
@@ -334,16 +396,22 @@ fn create_cylinder_section(app_state: Rc<RefCell<AppState>>, cylinder_count: usi
     };
     app_state.borrow_mut().cylinders.push(cylinder_config);
 
-    cylinder_section
+    grid.upcast::<gtk::Widget>() // Return the grid as a generic widget
 }
 
-#[allow(dead_code)]
 //Todo: refine this function
 fn create_cube_section(app_state: Rc<RefCell<AppState>>, cube_count: usize) -> gtk::Box {
+    let provider = CssProvider::new();
+    provider
+        .load_from_path("src/gui/style.css")
+        .expect("Failed to load CSS");
+
     let cube_section = gtk::Box::new(gtk::Orientation::Horizontal, 0);
     let label_text = format!("Cube {}", cube_count);
     let cube_label = gtk::Label::new(Some(&label_text));
     cube_section.pack_start(&cube_label, false, false, 0);
+    let style_context = cube_label.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     let pos_x_entry = Entry::new();
     pos_x_entry.set_placeholder_text(Some("X Position"));
@@ -357,9 +425,16 @@ fn create_cube_section(app_state: Rc<RefCell<AppState>>, cube_count: usize) -> g
     pos_z_entry.set_placeholder_text(Some("Z Position"));
     cube_section.pack_start(&pos_z_entry, false, false, 0);
 
-    let size_entry = Entry::new();
-    size_entry.set_placeholder_text(Some("Size"));
-    cube_section.pack_start(&size_entry, false, false, 0);
+    let radius_entry = Entry::new();
+    radius_entry.set_placeholder_text(Some("Radius"));
+    cube_section.pack_start(&radius_entry, false, false, 0);
+
+    // Apply styles to entries
+    let entries = vec![&pos_x_entry, &pos_y_entry, &pos_z_entry, &radius_entry];
+    for entry in entries {
+        let style_context = entry.get_style_context();
+        style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
+    }
 
     let material_selector = ComboBoxText::new();
     material_selector.append_text("Lambertian");
@@ -368,11 +443,15 @@ fn create_cube_section(app_state: Rc<RefCell<AppState>>, cube_count: usize) -> g
     material_selector.set_active(Some(0));
     cube_section.pack_start(&material_selector, false, false, 0);
 
+    // Apply styles to ComboBoxText
+    let style_context = material_selector.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
+
     let cube_config = CubeConfig {
         pos_x_entry: Rc::new(RefCell::new(pos_x_entry)),
         pos_y_entry: Rc::new(RefCell::new(pos_y_entry)),
         pos_z_entry: Rc::new(RefCell::new(pos_z_entry)),
-        size_entry: Rc::new(RefCell::new(size_entry)),
+        radius_entry: Rc::new(RefCell::new(radius_entry)),
         material_selector: Rc::new(RefCell::new(material_selector)),
     };
     app_state.borrow_mut().cubes.push(cube_config);
@@ -380,11 +459,21 @@ fn create_cube_section(app_state: Rc<RefCell<AppState>>, cube_count: usize) -> g
     cube_section
 }
 
-fn create_flat_plane_section(app_state: Rc<RefCell<AppState>>, plane_count: usize) -> gtk::Box {
+fn create_flat_plane_section(
+    app_state: Rc<RefCell<AppState>>,
+    flat_plane_count: usize,
+) -> gtk::Box {
+    let provider = CssProvider::new();
+    provider
+        .load_from_path("src/gui/style.css")
+        .expect("Failed to load CSS");
+
     let flat_plane_section = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    let label_text = format!("Flat Plane {}", plane_count);
+    let label_text = format!("Flat Plane {}", flat_plane_count);
     let flat_plane_label = gtk::Label::new(Some(&label_text));
     flat_plane_section.pack_start(&flat_plane_label, false, false, 0);
+    let style_context = flat_plane_label.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 
     let pos_x_entry = Entry::new();
     pos_x_entry.set_placeholder_text(Some("X Position"));
@@ -402,12 +491,23 @@ fn create_flat_plane_section(app_state: Rc<RefCell<AppState>>, plane_count: usiz
     radius_entry.set_placeholder_text(Some("Radius"));
     flat_plane_section.pack_start(&radius_entry, false, false, 0);
 
+    // Apply styles to entries
+    let entries = vec![&pos_x_entry, &pos_y_entry, &pos_z_entry, &radius_entry];
+    for entry in entries {
+        let style_context = entry.get_style_context();
+        style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
+    }
+
     let material_selector = ComboBoxText::new();
     material_selector.append_text("Lambertian");
     material_selector.append_text("Metal");
     material_selector.append_text("Dielectric");
     material_selector.set_active(Some(0));
     flat_plane_section.pack_start(&material_selector, false, false, 0);
+
+    // Apply styles to ComboBoxText
+    let style_context = material_selector.get_style_context();
+    style_context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_USER);
 
     let flat_plane_config = FlatPlaneConfig {
         pos_x_entry: Rc::new(RefCell::new(pos_x_entry)),
@@ -416,7 +516,7 @@ fn create_flat_plane_section(app_state: Rc<RefCell<AppState>>, plane_count: usiz
         radius_entry: Rc::new(RefCell::new(radius_entry)),
         material_selector: Rc::new(RefCell::new(material_selector)),
     };
-    app_state.borrow_mut().flat_planes.push(flat_plane_config);
+    app_state.borrow_mut().flat_plane.push(flat_plane_config);
 
     flat_plane_section
 }
