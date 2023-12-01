9081b5d791fff48d4aee4f08b5eac9eb3f86838c
Salam Foon <78011597+salam-github@users.noreply.github.com>
2023-12-01 16:50:36 +0200

    change Flat_planes name

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,11 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 fn main() {
-    // Shared state between GUI and main logic
-    // Initialize AppState with empty vectors
     let app_state = Rc::new(RefCell::new(AppState {
         spheres: vec![],
         cylinders: vec![],
         cubes: vec![],
-        flat_plane: vec![],
+        flat_planes: vec![],
     }));
-
-    // Pass AppState to launch_gui
     launch_gui(app_state.clone());
 }
